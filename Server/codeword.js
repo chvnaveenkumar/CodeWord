5f09c08f25b2c3ceb1b0aff26838fc0c1a7b20b1
NaveenKumar Nuggu <S531373@nwmissouri.edu>
2018-09-16 22:48:46 -0500

    Arranged Folder Structure

---
+++ Server/codeword.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+require('./config/database');
+var userRouter = require('./routes/user.route');
+var app = express();
+const bodyParser = require('body-parser');
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, '../Client/dist'));
+app.use(express.static(path.join(__dirname, '../Client/dist')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/codeword', userRouter);
+
+
+module.exports = app;
