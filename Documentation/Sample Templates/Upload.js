04eac8e0368812e41f9adc00e392585b11848cec
SRAVYA KANCHARLA <35503094+SRAVYAKANCHARLA@users.noreply.github.com>
2018-09-30 22:02:08 -0500

    Upload file code

--- Documentation/Sample Templates/Upload.js
+++ Documentation/Sample Templates/Upload.js
@@ -60,3 +60,24 @@ http.createServer(function (req, res) {
   }
 }).listen(8080);
 
+
+
+var x = document.getElementById("myFile");
+var txt = "";
+if ('files' in x) {
+    if (x.files.length == 0) {
+        txt = "Select one or more files.";
+    } else {
+        for (var i = 0; i < x.files.length; i++) {
+            txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+            var file = x.files[i];
+            if ('name' in file) {
+                txt += "name: " + file.name + "<br>";
+            }
+            if ('size' in file) {
+                txt += "size: " + file.size + " bytes <br>";
+            }
+        }
+    }
+} 
+document.getElementById ("demo").innerHTML = txt;
