0168064c60aa554ba15b3e522987610ac16b774d
vyshnaviyreddy <35476682+vyshnaviyreddy@users.noreply.github.com>
2018-09-30 19:45:24 -0500

    modified changes of UI  in  ForgetPassword.vue

--- Client/src/components/ForgetPassword.vue
+++ Client/src/components/ForgetPassword.vue
@@ -1,35 +1,76 @@
 <template>
-<div class="container">
-        <div class="row" style="margin-top: 7em">
-            <div class="col-md-3 col-lg-3 col-xs-1 col-sm-1"></div>
-            <div class="card col-md-6 col-lg-6 col-xs-10 col-sm-10">
-                <div class="card-body">
-                        <div style="text-align: center">       
-                            </div>
-                            <div>
-                                <div class="alert alert-success" v-if="signed && msg" role="alert">
-                                       {{ msg }}
-                                </div>
-                                <div class="alert alert-danger" v-else-if="!signed && msg" role="alert">
-                                      {{ msg }}
-                                </div>
-                                <form>
-                                    <div class="form-group row " style="padding-left: 10px;padding-right: 10px">
-                                    <label for="inputEmail" style="font-family: inherit">Email Address</label>
-                                    <input type="text" name="emailID" class="form-control" id="inputEmail" placeholder="Email ID">
-                                    </div>
-                                    <div class="form-group" :class="{invalid: $v.email.$error}">
-                                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" required="required" pattern=".+@*.edu" v-model="email">
-                                        <p v-if="!$v.email.isUnique">This email id already registered!!</p>
-                                    </div>
-                                    <button type="button" class="btn btn-primary btn-lg btn-block" @click="forget">Get Password</button>
-                                    </form>
-                                </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-lg-3 col-xs-1 col-sm-1"></div>
-        </div>
-    </div>
+
+<div
+class="container">
+
+<div
+class="row"
+style="margin-top: 7em">
+
+<div
+class="col-md-3 col-lg-3 col-xs-1 col-sm-1"></div>
+
+<div
+class="card col-md-6 col-lg-6 col-xs-10 col-sm-10">
+
+<div
+class="card-body">
+
+<div
+style="text-align: center">
+
+</div>
+
+<div>
+
+<div
+class="alert alert-success"
+v-if="signed &&
+msg" role="alert"> {{
+msg }} </div>
+
+<div
+class="alert alert-danger"
+v-else-if="!signed &&
+msg" role="alert"> {{
+msg }} </div>
+
+<form>
+
+<div
+class="form-group">
+
+<input
+type="email"
+class="form-control"
+placeholder="Enter email"
+required="required"
+pattern=".+@*.edu"
+v-model="email"
+@blur="$v.email.$touch()">
+
+</div>
+
+<button
+type="button"
+class="btn btn-primary btn-lg btn-block"
+@click="forget">Get Password</button>
+
+</form>
+
+</div>
+
+</div>
+
+</div>
+
+<div
+class="col-md-3 col-lg-3 col-xs-1 col-sm-1"></div>
+
+</div>
+
+</div>
+
 </template>
 
 <script>
