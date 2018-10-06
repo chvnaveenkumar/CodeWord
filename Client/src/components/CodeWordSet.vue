69bf82f57b8a23de4f1fb25c113f684e7b19a08b
vyshnaviyreddy <35476682+vyshnaviyreddy@users.noreply.github.com>
2018-10-06 15:28:59 -0500

    Worked on CodeWordSet.vue

---
+++ Client/src/components/CodeWordSet.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container-fluid" style="margin-top:5em" >
+      <div class="row">
+      <div class="col-md-2 col-lg-2 col-xs-0 col-sm-0">
+      </div>
+      <button type="button" class="btn btn-primary fa fa-plus" data-toggle="modal" data-target="#createCodeWordSet">
+       Create CodeWord Set
+      </button>
+<!-- Modal -->
+<div class="modal fade" id="createCodeWordSet" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add New CodeWord</h5>
+      </div>
+      <div class="modal-body">
+   <form>
+  <div class="form-group" >
+    <label for="formGroupExampleInput" style="float:left">CodeWord Set Name</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter CodeWord Set Name">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlFile1" style="float:left" >Upload CodeWord Set(Excel)</label>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Upload CodeWord Set </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+      </div>
+    </div>
+</template>
+
+
+<style>
+#message{
+    margin-top:5em;
+}
+</style>
