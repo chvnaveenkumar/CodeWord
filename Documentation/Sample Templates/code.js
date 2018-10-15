b1c3183252ea2a889e7c53b34e6e915692d977f8
SRAVYA KANCHARLA <35503094+SRAVYAKANCHARLA@users.noreply.github.com>
2018-10-14 19:18:15 -0500

    Assigning codeword to student

---
+++ Documentation/Sample Templates/code.js
@@ -0,0 +1,131 @@
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+    var range = sheet.getRange("B2:C5");
+    range.load("address");
+
+    return context.sync()
+        .then(function () {
+            console.log(`The address of the range B2:C5 is "${range.address}"`);
+        });
+}).catch(errorHandlerFunction);
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+    var range = sheet.getRange("MyRange");
+    range.load("address");
+
+    return context.sync()
+        .then(function () {
+            console.log(`The address of the range "MyRange" is "${range.address}"`);
+        });
+}).catch(errorHandlerFunction);
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+    var range = sheet.getUsedRange();
+    range.load("address");
+
+    return context.sync()
+        .then(function () {
+            console.log(`The address of the used range in the worksheet is "${range.address}"`);
+        });
+}).catch(errorHandlerFunction);
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+    var range = sheet.getRange();
+    range.load("address");
+
+    return context.sync()
+        .then(function () {
+            console.log(`The address of the entire worksheet range is "${range.address}"`);
+        });
+}).catch(errorHandlerFunction);
+
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+    var range = sheet.getRange("B4:E4");
+
+    range.insert(Excel.InsertShiftDirection.down);
+
+    return context.sync();
+}).catch(errorHandlerFunction);
+
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+    var range = sheet.getRange("E2:E5");
+
+    range.clear();
+
+    return context.sync();
+}).catch(errorHandlerFunction);
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+    var range = sheet.getRange("B4:E4");
+
+    range.delete(Excel.DeleteShiftDirection.up);
+
+    return context.sync();
+}).catch(errorHandlerFunction);
+
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getActiveWorksheet();
+    var range = sheet.getRange("B2:E6");
+
+    range.select();
+
+    return context.sync();
+}).catch(errorHandlerFunction);
+
+
+Excel.run(function (context) {
+    var range = context.workbook.getSelectedRange();
+    range.load("address");
+
+    return context.sync()
+        .then(function () {
+            console.log(`The address of the selected range is "${range.address}"`);
+        });
+}).catch(errorHandlerFunction);
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+
+    var range = sheet.getRange("E3");
+    range.formulas = [[ "=C3 * D3" ]];
+    range.format.autofitColumns();
+
+    return context.sync();
+}).catch(errorHandlerFunction);
+
+
+Excel.run(function (context) {
+    var sheet = context.workbook.worksheets.getItem("Sample");
+
+    var data = [
+        ["=C3 * D3"],
+        ["=C4 * D4"],
+        ["=C5 * D5"],
+        ["=SUM(E3:E5)"]
+    ];
+
+    var range = sheet.getRange("E3:E6");
+    range.formulas = data;
+    range.format.autofitColumns();
+
+    return context.sync();
+}).catch(errorHandlerFunction);
+
+
