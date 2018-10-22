2f149cb8e644b126d50cb5ba5850ae42bf5f06ae
NaveenKumar Nuggu <S531373@nwmissouri.edu>
2018-10-21 23:39:14 -0500

    Adding the modals for the card

--- Client/src/components/InstructorDashboard.vue
+++ Client/src/components/InstructorDashboard.vue
@@ -64,15 +64,90 @@
                     </div>
                 </div>
             </div>
-            <div class="card" style="width: 20rem; margin-left: 3em;background-color:#41f4b2">
+            <div class="card" style="width: 20rem; margin-left: 1em;background-color:#41f4b2">
         <div class="card-body">
           <h5 class="card-title">Mobile Computing - Android</h5>
           <h6 class="card-subtitle mb-2 text-muted">44644-01</h6>
           <pre>10/21/2018      12/21/2018</pre>          
           <a href="#" class="card-link">Survey Start URL</a>
           <a href="#" class="card-link">Survey End URL</a>
-          <a href="#" class="card-link"><i class="fas fa-eye"></i> </a>
-          <a href="#" class="card-link"><i class="fas fa-trash"></i></a>
+          <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Viewcoursemodal">
+  <i class="fas fa-eye"></i>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="Viewcoursemodal" tabindex="-1" role="dialog" aria-labelledby="#ViewcoursemodalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="ViewcoursemodalLabel">View Course Details</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">StudentName</th>
+      <th scope="col">Student Email</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Naveenkumar Nuggu</td>
+      <td>S531373@nwmissouri.edu</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Naveenkumar Chandaluri</td>
+      <td>S530742@nwmissouri.edu</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>SriLekha Vijaya </td>
+      <td>S531382@nwmissouri.edu</td>
+      
+    </tr>
+  </tbody>
+</table>
+      </div>
+     
+    </div>
+  </div>
+</div>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  <i class="fas fa-trash"></i>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Delete Course</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Are you sure you want to delete the course?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-warning" data-dismiss="modal">Yes</button>
+        <button type="button" class="btn btn-primary">No</button>
+      </div>
+    </div>
+  </div>
+</div>
+          
         </div>
       </div></div>
             
