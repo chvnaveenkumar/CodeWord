21ae230296cdf3b2d65d853f5e94532fff395f74
Chaithanya Cherukuru <S531495@nwmissouro.edu>
2018-10-22 23:38:17 -0500

    POP

---
+++ Client/data/popup.js
@@ -0,0 +1,32 @@
+<h2>Dropdown Menu</h2>
+<p>Move the mouse over the button to open the dropdown menu.</p>
+ <div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Logout</a>
+    <a href="#">codeword</a>
+   
+  </div>
+  
+<div class="btn-group dropup">
+    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Dropup
+    </button>
+    <div class="dropdown-menu">
+      
+    </div>
+  </div>
+  
+  
+  <div class="btn-group dropup">
+    <button type="button" class="btn btn-secondary">
+      Split dropup
+    </button>
+    <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <span class="sr-only">Toggle Dropdown</span>
+    </button>
+    <div class="dropdown-menu">
+     
+    </div>
+  </div>
+</div>
