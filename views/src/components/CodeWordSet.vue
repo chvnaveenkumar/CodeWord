cd97f661512e4b398b33c2a94ecbeb8a974d7f11
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2018-10-24 12:46:33 -0500

    updated CodeWordSet.vue

---
+++ views/src/components/CodeWordSet.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+      <div class="row">
+      <div class="col-md-2 col-lg-2 col-xs-0 col-sm-0"></div>
+      <div class="table-responsive col-md-8 col-lg-8 col-xs-12 col-sm-12" style="margin-top:5em">
+    
+      <h2>Codeword sets</h2>
+           
+  
+    </div>
+    <div class="col-md-2 col-lg-2 col-xs-0 col-sm-0">
+        <a href="">
+            Hoot Original set
+        </a>
+
+        <a href="">
+            Large set
+        </a>
+
+    </div>
+    </div></div>
+</template>
+
+
+<style>
+#message{
+    margin-top:5em;
+}
+</style>
+
+
+
+
