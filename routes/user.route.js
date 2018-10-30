826a222ead058799755dcd91276e566887778204
Srimai <S531384@nwmissouri.edu>
2018-10-30 17:57:15 -0500

    added routes to codewordset

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var usersController = require('../controllers/controller.user')
 var courseController = require('../controllers/controller.course')
+var codewordsetController = require('../controllers/controller.codewordset')
 const bodyParser = require('body-parser');
 let multer = require('multer')
 
@@ -19,4 +20,5 @@ router.post('/validateEmail', usersController.validateEmail);
 router.post('/sendmail', usersController.tempPassword);
 router.post('/changepassword', usersController.changePassword);
 router.post('/addnewCourse', courseController.addCourse);
+router.post('/addcodewordset',codewordsetController.addcodewordset)
 module.exports = router;
