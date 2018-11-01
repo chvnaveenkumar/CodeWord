3ca60abc6a9e7cece99d74415aac1e1e65505545
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2018-10-31 19:46:18 -0500

    Created UI for Create CodeWordSet where the Instructor would be able to view default CodewordSet, would be able to create new CodewordSet and also would be able to view added CodewordSet along with Count of Codewords in that set and Email id of Instructor creating that CodewordSet.

--- views/src/components/CodeWordSet.vue
+++ views/src/components/CodeWordSet.vue
@@ -1,33 +1,82 @@
 <template>
-    <div class="container">
-      <div class="row">
-      <div class="col-md-2 col-lg-2 col-xs-0 col-sm-0"></div>
-      <div class="table-responsive col-md-8 col-lg-8 col-xs-12 col-sm-12" style="margin-top:5em">
+<div class="container-fluid" style="margin-top:5em" >
+            <div class="col-md-4 col-lg-4 col-xs-0 col-sm-0">
+                <button type="button" class="btn btn-success" title="Create CodeWord Set" data-toggle="modal" data-target="#addcourse">
+                    <span class="fa fa-plus"></span> Create Codeword Set
+                </button>
+                <div class="modal fade" id="addcourse" tabindex="-1" role="dialog" aria-labelledby="addcourseLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content" style= "width:fit-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="addcourseLabel">Create CodewordSet</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Enter CordwordSet Name" data-toggle="tooltip" data-placement="top" title="Enter CordwordSet Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="number" class="form-control" id="replyNumber" data-bind="value:replyNumber" placeholder="Enter Number of Codewords" data-toggle="tooltip" data-placement="top" title="Enter Number of Codewords">
+                                    </div>
+                                    <div class="form-group text-left">
+                                            Upload Cordword Set(Excel)
+                                            <input type="file" class="form-control-file" id="exampleFormControlFile1" style="margin-top:1em">
+                                        </div>
+                                    
+                                    
+                                </form>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                                <button type="button" class="btn btn-success">Create Codeword Set</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col"><h4><u>ID</u></h4></th>
+      <th scope="col"><h4><u>CodewordSet Name</u></h4></th>
+      <th scope="col"><h4><u>Codeword Count</u></h4></th>
+      <th scope="col"><h4><u>CordwordSet Creator</u></h4></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Hoot Original set</td>
+      <td>80</td>
+      <td>hoot@nwmissouri.edu</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Large set</td>
+      <td>200</td>
+      <td>dcase@nwmissouri.edu</td>
+    </tr>
     
-      <h2>Codeword sets</h2>
-           
+  </tbody>
+</table>      
   
-    </div>
-    <div class="col-md-2 col-lg-2 col-xs-0 col-sm-0">
-        <a href="">
-            Hoot Original set
-        </a>
+        </div>
 
-        <a href="">
-            Large set
-        </a>
-
-    </div>
-    </div></div>
+      </div>
+      </div>
+      
+    
 </template>
 
 
 <style>
-#message{
-    margin-top:5em;
+#message {
+  margin-top: 5em;
 }
 </style>
-
-
-
 
