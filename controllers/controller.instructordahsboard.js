cf910a2817abe3d7eb596069e036f55552b58ed7
SRAVYA KANCHARLA <35503094+SRAVYAKANCHARLA@users.noreply.github.com>
2018-11-04 21:44:41 -0600

    Updated Instructor dashboard code

--- controllers/controller.instructordahsboard.js
+++ controllers/controller.instructordahsboard.js
@@ -2,28 +2,14 @@
  * @author Sravya Kancharla <S531500@nwmissouri.edu>
  */
 
-let addedcourse = (req,res) => {
+let getcourse = (req,res) => {
     var body = _.pick(req.body,['courseNameKey','email','codeWordSet','Startdate','Enddate','PreSurveyURL','PostSurveyURL']);
 
     courseModel.findOne({courseName: body.courseNameKey}, function (err, course) {
         if(err){
             return res.json({ code: 200, message: 'Course Doesnt Exist'});
         }
-        
-        return bcrypt.compare(body.courseNameKey,course.courseNameKey,(err,result) => {
-            if(result){
-                var newToken = jwt.course({courseName: body.courseNameKey, codeWordSet: course.codeWordSet,  Startdate: course.Startdate, Enddate: course.Enddate, PreSurveyURL : course.PreSurveyURL, PostSurveyURL: course.PostSurveyURL },'codewordnwmsu',{expiresIn:  10000 * 3000 }).toString();
-
-                courseModel.updateOne({courseName: body.courseNameKey, codeWordSet: course.codeWordSet,  Startdate: course.Startdate, Enddate: course.Enddate, PreSurveyURL : course.PreSurveyURL, PostSurveyURL: course.PostSurveyURL},{$set: {token: newToken}}, (err) =>{
-                    if(err){
-                        return res.json({ code: 200, message: 'Unable to generate and update Token'});
-                    }
-                    return res.json({ code: 200, message: 'Course Added Successfully. Redirecting.', token: newToken, email : course.email });
-                })
-            }else{
-                return res.json({ code: 200, message: "Invalid User!!"})
-            }
-        })
+        return course;
     })
 }
-module.exports.addedcourse = addedcourse;
+module.exports.getcourse = getcourse;
--- controllers/controller.studentdashboard.js
+++ controllers/controller.studentdashboard.js
@@ -2,4 +2,9 @@
  * @author Sravya Kancharla <S531500@nwmissouri.edu>
  */
 
- 
+ let studentDetails = (req,res) => {
+    var body = _.pick(req.body,['Acknowledged','CourseNameKey','EmailKey','StudentName']);
+    courseStudentModel.findOne({Acknowledged: body.Acknowledged})
+    
+ }
+module.exports.studentDetails = studentDetails;
