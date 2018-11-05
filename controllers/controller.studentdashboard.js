38e697c1cf932361a92a11a02358cc774e075402
SRAVYA KANCHARLA <35503094+SRAVYAKANCHARLA@users.noreply.github.com>
2018-11-05 09:31:05 -0600

    Student Dashboad is now sorted with EmailKey

--- controllers/controller.studentdashboard.js
+++ controllers/controller.studentdashboard.js
@@ -3,8 +3,8 @@
  */
 
  let getstudentDetails = (req,res) => {
-    var body = _.pick(req.body,['Acknowledged','CourseNameKey','EmailKey','StudentName']);
-    courseStudentModel.findOne({Acknowledged: body.Acknowledged}), function (err, course) {
+    var body = _.pick(req.body,['EmailKey']);
+    courseStudentModel.findOne({EmailKey: body.EmailKey}), function (err, course) {
             if(err){
                 return res.json({ code: 200, message: 'Course Student not exist'});
             }
