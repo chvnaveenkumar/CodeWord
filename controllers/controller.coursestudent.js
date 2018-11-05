f964902c851691353448bf2fe186a89a39b44268
vyshnaviyreddy <S531383@nwmissouri.edu>
2018-11-04 20:48:57 -0600

    Updated controller.coursestudent.js

--- controllers/controller.coursestudent.js
+++ controllers/controller.coursestudent.js
@@ -5,15 +5,12 @@ var { CourseStudentModel } = require('../model/model.coursestudent');
 var { mongoose } = require('./../config/database')
 var { CodeWord } = require('../model/model.codeword')
 let XLSX = require('xlsx')
-
 module.exports.addCourseStudent = addCourseStudent;
-
 let addCourseStudent = (req,res) => {
     var codewordslist = [];
     var studentIDList = [];
     var studentNameList = [];
     var studentData = [];
-
     var workbook = XLSX.read(req.file.buffer, {type:"buffer"})
     _.each(XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]), function(ele,index,list){
     var status = true;
@@ -23,24 +20,24 @@ let addCourseStudent = (req,res) => {
     studentIDList.push(ele[firstKey]);
     status = false
     }else {
-    studentNameList.push('Name '+ele[firstKey]);
+    studentNameList.push(ele[firstKey]);
     }
     }
   })
     var body = _.pick(req.body,['CourseNameKey','CodeWordSetName']);    
-    console.log(body.CourseNameKey);
-    console.log(body.CodeWordSetName);
+    //console.log(body.CourseNameKey);
+    //console.log(body.CodeWordSetName);
 
     CodeWord.findOne({CodeWordSetName: body.CodeWordSetName}, function (err, CodeWords) {
         if(err){
             return res.json({ code: 200, message: 'Email id not registered!!'});
         }
         codewordslist.push(CodeWords.codeword)
-        console.log(CodeWords.codeword)
+        //console.log(CodeWords.codeword)
     })
-    console.log("studentID"+studentIDList);
-    console.log("studentName"+studentNameList);
-    console.log(codewordslist);
+    //console.log("studentID"+studentIDList);
+    //console.log("studentName"+studentNameList);
+    //console.log(codewordslist);
 
     var shuffleCodeWords = shuffle(codewordslist);
 
