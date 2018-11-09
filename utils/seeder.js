c5cbd0b60b45854382fba2298c28ba234ea241db
ANURAG KUMAR <s531372@nwmissouri.edu>
2018-11-09 09:19:49 -0600

    seeder updated

--- utils/seeder.js
+++ utils/seeder.js
@@ -5,55 +5,51 @@ const datacourse = require('../data/data.Course.json')
 const datacodeword = require('../data/data.codeword.json')
 const user = require('../data/data.user.json')
 
-const dataCodewordset=require('../data/data.codewordset.json')
-const dataCoursestudent=require('../data/data.coursestudent.json') 
+const dataCodewordset = require('../data/data.codewordset.json')
+const dataCoursestudent = require('../data/data.coursestudent.json')
 
 
 const LOG = require('../utils/logger.js')
 
 module.exports = (app) => {
-    LOG.info('START seeder.')
-    const db = {}
+  LOG.info('START seeder.')
+  const db = {}
 
 
-    users.forEach((user) => {
-        userController.newUser(user)
-      })
+  users.forEach((user) => {
+    userController.newUser(user)
+  })
 
 
-    db.datacourse = new Datastore()
-  db.datacourse.loadDatabase()
-
-  // insert the sample data into our data store
-  db.datacourse.insert(datacourse)
-
-
-
-  db.datacodeword = new Datastore()
-  db.datacodeword.loadDatabase()
-
-  // insert the sample data into our data store
-  db.datacodeword.insert(datacodeword)
-
-
-
+//USER
   db.datauser = new Datastore()
   db.datauser.loadDatabase()
-
-  // insert the sample data into our data store
   db.datauser.insert(datauser)
 
 
-db.dataCodewordset = new Datastore()
-db.dataCodewordset.loadDatabase()
-db.dataCodewordset.insert(dataCodewordset)
-
-db.dataCoursestudent = new Datastore()
-db.dataCoursestudent.loadDatabase()
-db.dataCoursestudent.insert(dataCoursestudent)
+//CODEWORDSET
+  db.dataCodewordset = new Datastore()
+  db.dataCodewordset.loadDatabase()
+  db.dataCodewordset.insert(dataCodewordset)
 
 
+ //CODEWORD
+ 
+ db.datacodeword = new Datastore()
+ db.datacodeword.loadDatabase()
+ db.datacodeword.insert(datacodeword)
 
+ //COURSE
+
+  db.datacourse = new Datastore()
+  db.datacourse.loadDatabase()
+  db.datacourse.insert(datacourse)
+
+//COURSESTUDENT
+
+  db.dataCoursestudent = new Datastore()
+  db.dataCoursestudent.loadDatabase()
+  db.dataCoursestudent.insert(dataCoursestudent)
 
   app.locals.users = db.users.find(users)
   LOG.debug(`${app.locals.users.query.length} users registered`)
