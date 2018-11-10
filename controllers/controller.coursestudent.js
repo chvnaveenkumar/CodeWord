b9ba7f366f9970f12e5fb7064a0cfb7dffd5f203
vyshnaviyreddy <S531383@nwmissouri.edu>
2018-11-10 16:18:31 -0600

    completed coursestudent controller

--- controllers/controller.coursestudent.js
+++ controllers/controller.coursestudent.js
@@ -6,68 +6,48 @@ var { mongoose } = require('./../config/database')
 var { CodeWord } = require('../model/model.codeword')
 let XLSX = require('xlsx')
 let addCourseStudent = (req,res) => {
-    var codewordslist = [];
-    var studentIDList = [];
-    var studentNameList = [];
-    var studentData = [];
-    var workbook = XLSX.read(req.file.buffer, {type:"buffer"})
-    _.each(XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]), function(ele,index,list){
-    var status = true;
-    for(firstKey in ele) {
-    //studentData.push(ele[firstKey])
-    if( status == true ) {
-    studentIDList.push(ele[firstKey]);
-    status = false
-    }else {
-    studentNameList.push(ele[firstKey]);
-    }
-    }
-  })
+    var codewordslist =[];
+    var shuffleCodeWords, studetList;
+    var studentidList=[],studentNameList=[];
+    var workbook = XLSX.read(req.file.buffer, {type:"buffer"});
     var body = _.pick(req.body,['CourseNameKey','CodeWordSetName']);    
-    //console.log(body.CourseNameKey);
-    //console.log(body.CodeWordSetName);
-
+    studetList = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+    
     CodeWord.findOne({CodeWordSetName: body.CodeWordSetName}, function (err, CodeWords) {
         if(err){
-            return res.json({ code: 200, message: 'Email id not registered!!'});
+            return res.json({ code: 204, message: 'CodeWord Set Not found error'});
         }
         codewordslist.push(CodeWords.codeword)
-        //console.log(CodeWords.codeword)
+        if(codewordslist.length < studetList.length )
+        {
+            return res.json({ code: 200, message: 'Insufficent Codewords'})
+        }else {
+            shuffleCodeWords = shuffle(codewordslist);
+            for(var i=0;i<studetList.length;i++) {
+            var studentData = JSON.parse(JSON.stringify(studetList[i]))
+            studentidList.push(studentData[Object.keys(studentData)[0]])
+            studentNameList.push(studentData[Object.keys(studentData)[1]])
+            }
+
+            console.log(studentidList+" "+studentNameList)
+
+            for(var i=0;i<studentidList.length;i++){
+                var courseStudentModel = CourseStudentModel({
+                    CourseNameKey: body.CourseNameKey,
+                    EmailKey:studentidList[i],
+                    Codeword:'Codewords',
+                    StudentName: studentNameList[i],
+                    Acknowledged: false 
+                }); 
+                courseStudentModel.save();
+             }           
+        }
+    }).then((user) => {
+        if(user)
+        return res.json({ code: 200, message: true});           
+    }).catch((e) => {
+        return res.json({ code: 400, message: e});        
     })
-    //console.log("studentID"+studentIDList);
-    //console.log("studentName"+studentNameList);
-    //console.log(codewordslist);
-
-    var shuffleCodeWords = shuffle(codewordslist);
-
-    // convert xlsx to json
-        var server = http.createServer(function(request, response) {
-        var XLSX = require('xlsx');
-        var workbook = XLSX.readFile('file.xlsx');
-        var sheet_name_list = workbook.SheetNames;
-        var testList = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
-        console.log(typeof testList)
-        for (var i in testList){
-            testList[i].fileName = "file.xlsx"
-        }  
-        console.log(testList)
-    })
-
-    // var courseStudentModel = new CourseStudentModel({
-    //     CourseNameKey: body.CourseNameKey,
-    //     EmailKey:body.EmailKey,
-    //     Codeword:body.Codeword,
-    //     StudentName: body.name,
-    //     Acknowledged:body.Acknowledged
-    // });
-    // courseStudentModel.save().then((user) => {
-    //     if(user)
-    //     return res.json({ code: 200, message: true});
-
-    // }).catch((e) => {
-    //     console.log(e);
-    //     return res.json({ code: 400, message : e});
-    // })
 }
 
 function shuffle(array) {
