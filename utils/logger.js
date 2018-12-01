ab699ee8e2a3bf1b31ccba936d93e657d970b070
ANURAG KUMAR <s531372@nwmissouri.edu>
2018-12-01 11:47:24 -0600

    Seeder and logger has been updated

--- utils/logger.js
+++ utils/logger.js
@@ -12,11 +12,13 @@ const logger = new (winston.Logger)({
   level: 'debug',
   transports: [
     new (winston.transports.Console)({ json: false, timestamp: true }),
-    //new winston.transports.File({ filename: path.join(logDir, '/debug.log'), json: false })
+    new winston.transports.File({ filename: path.join(logDir, '/debug.log'), json: false })
+   
   ],
   exceptionHandlers: [
     new (winston.transports.Console)({ json: false, timestamp: true, humanReadableUnhandledException: true }),
-    //new winston.transports.File({ filename: path.join(logDir, '/debug.log'), json: false, humanReadableUnhandledException: true })
+    new winston.transports.File({ filename: path.join(logDir, '/debug.log'), json: false, humanReadableUnhandledException: true })
+    
   ],
   exitOnError: false
 })
--- utils/seeder.js
+++ utils/seeder.js
@@ -6,6 +6,8 @@ const datacodeword = require('../data/data.codeword.json')
 const user = require('../data/data.user.json')
 const dataCodewordset = require('../data/data.codewordset.json')
 const dataCoursestudent = require('../data/data.coursestudent.json')
+const hootoriginal=require('../data/hootoriginal.json')
+const largeset=require('../data/largeset.json')
 const LOG = require('./logger.js')
 
 module.exports = (app) => {
@@ -13,14 +15,14 @@ module.exports = (app) => {
   const db = {}
 
 
-/*   users.forEach((user) => {
-    userController.newUser(user)
-  })
- */
-//USER
-  // db.datauser = new Datastore()
-  // db.datauser.loadDatabase()
-  // db.datauser.insert(datauser)
+//    users.forEach((user) => {
+//     userController.newUser(user)
+//   })
+ 
+// //USER
+//   db.datauser = new Datastore()
+//   db.datauser.loadDatabase()
+//   db.datauser.insert(datauser)
 
 //CODEWORDSET
   db.dataCodewordset = new Datastore()
@@ -46,7 +48,16 @@ module.exports = (app) => {
   db.dataCoursestudent.loadDatabase()
   db.dataCoursestudent.insert(dataCoursestudent)
 
+  //hootoriginal
+  db.hootoriginal = new Datastore()
+  db.hootoriginal.loadDatabase()
+  db.hootoriginal.insert(hootoriginal)
 
+  //largeset
+
+  db.largeset = new Datastore()
+  db.largeset.loadDatabase()
+  db.largeset.insert(largeset)
 
   // app.locals.datauser = db.datauser.find(datauser)
   // LOG.debug(`${app.locals.datauser.query.length} datauser seeded`)
@@ -66,6 +77,12 @@ module.exports = (app) => {
   app.locals.dataCoursestudent = db.dataCoursestudent.find(dataCoursestudent)
   LOG.debug(`${app.locals.dataCoursestudent.query.length} dataCoursestudent seeded`)
 
+  app.locals.hootoriginal = db.hootoriginal.find(hootoriginal)
+  LOG.debug(`${app.locals.hootoriginal.query.length} hootoriginal seeded`)
+
+  
+  app.locals.largeset = db.largeset.find(largeset)
+  LOG.debug(`${app.locals.largeset.query.length} largeset seeded`)
   
   LOG.info('END Seeder. Sample data read and verified.')
 }
